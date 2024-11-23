d7a39b890057caef4d601383ab3b43b50c63f9e6
Nikolay Dimov <nikolay.dimov@yara.com>
2024-11-24 00:28:44 +0200

    feat: processFunction use uuid

--- src/processFunction.ts
+++ src/processFunction.ts
@@ -1,25 +1,28 @@
 import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
 import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { v4 } from "uuid";
 
 const snsClient = new SNSClient({});
 const dynamoClient = new DynamoDBClient({});
 
 export const handler = async (event: any) => {
+    const tableName = process.env.TABLE_NAME;
+    const topicArn = process.env.TOPIC_ARN;
+
     const body = JSON.parse(event.body);
-    const isValidJson = body.text ? true : false; // Just a simple check for validity
 
     console.log("Event body:", event.body); // Debugging log
     console.log("Parsed body:", body); // Debugging log
 
-    if (!isValidJson) {
+    if (!body || !body.text) {
         console.log("Invalid JSON detected");
 
         // Add an item to DynamoDB with the invalid JSON message
         const ttl = Math.floor(Date.now() / 1000) + 3600; // TTL example: 1 hour
         const putItemParams = {
-            TableName: process.env.TABLE_NAME,
+            TableName: tableName,
             Item: {
-                id: { S: new Date().toISOString() },
+                id: { S: v4() },
                 errorMessage: { S: "Invalid JSON" },
                 ttl: { N: ttl.toString() },
             },
@@ -34,7 +37,7 @@ export const handler = async (event: any) => {
         // Send an SNS message
         const message = `Valid JSON received: ${body.text}`;
         const snsParams = {
-            TopicArn: process.env.TOPIC_ARN,
+            TopicArn: topicArn,
             Message: message,
         };
 
